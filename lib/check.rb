19b7a35ce8ddcc22866fc71e98e18215630fe60a
nikbo <nikita-bobrik@yandex.ru>
2013-03-10 00:41:35 +0300

    Fix logic

--- lib/check.rb
+++ lib/check.rb
@@ -5,14 +5,21 @@ class Check
   def checking
     b=[]
     @players.each  {|player| b<<player.points}
-    if b[0]==b[b.length-1]
+    if b[0]>21 && b[1]>21
+      puts "You are lose((("
+    else
+      if b[0]>21 && b[1]<=21
+        puts "Gulec 2, you win!!!"
+    elsif b[0]<=21 && b[1]>21
+          puts "Gulec 2, you win!!!"
+    elsif b[0]==b[b.length-1]
       puts "Ups, drawn game!"
-    elsif (b[0]-21).abs > (b[b.length-1]-21).abs
-       puts "Gulec 2, you win!!!"
-    elsif (b[0]-21).abs < (b[b.length-1]-21).abs
+    elsif b[0]>b[1]
        puts "Gulec 1, you win!!!"
+    elsif b[0]<b[1]
+       puts "Gulec 2, you win!!!"
     end
-
+    end
 
     end
   end
